95085b40e62e8be3b1bb74605409ab885e8ece38
iordache <mihai.iordache82@gmail.com>
2024-07-11 18:16:57 +0300

    initiating project

---
+++ gRPC-core/src/main/java/com/gini/GRpcCoreApplication.java
@@ -0,0 +1,13 @@
+package com.gini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcCoreApplication.class, args);
+    }
+
+}
