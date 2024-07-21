31991e5ab5f6e8be2bc08e75b0592115074d363f
iordache <mihai.iordache82@gmail.com>
2024-07-21 20:32:39 +0300

    initiating api service

---
+++ gRPC-api/src/main/java/com/gini/GRpcApiApplication.java
@@ -0,0 +1,13 @@
+package com.gini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcApiApplication.class, args);
+    }
+
+}
