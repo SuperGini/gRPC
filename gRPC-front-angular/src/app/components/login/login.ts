cc2def737887fa4cddec4a9740479e5e389234d3
iordache <mihai.iordache82@gmail.com>
2024-08-05 23:25:34 +0300

    added login page

--- gRPC-front-angular/src/app/components/login/login.ts
+++ gRPC-front-angular/src/app/components/login/login.ts
@@ -1,9 +1,13 @@
 import {Component} from "@angular/core";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
     selector: "login-component",
     templateUrl: "./login.html",
     styleUrl: "./login.css",
+    imports: [
+        NgOptimizedImage
+    ],
     standalone: true
 })
 export class Login {
